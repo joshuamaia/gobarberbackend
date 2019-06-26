a0ab95bca52b2ecdb9c20ae9f5c1566f92e109ea
Joshua Maia Rodrigues <joshuamaia@codata.pb.gov.br>
2019-06-26 14:37:51 -0300

    Criando loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = Router();
 
-routes.get('/', (req, res) => {
-  res.json({ message: 'Hello Joshua' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Joshua Maia',
+    email: 'joshuamaia@gmail.com',
+    password_hash: '123',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
