e04f4be1fae6d373a2fd11ff8b3080ac41939f4c
Joshua Maia Rodrigues <joshuamaia@codata.pb.gov.br>
2019-06-26 17:18:19 -0300

    Cadastro de usuários

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Joshua Maia',
-    email: 'joshuamaia@gmail.com',
-    password_hash: '123',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
