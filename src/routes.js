b42b9f006ce136f2f6c188d17cc87189f9f86107
Joshua Maia Rodrigues <joshuamaia@codata.pb.gov.br>
2019-07-02 18:21:58 -0300

    Listando agendamentos do usuário

--- src/routes.js
+++ src/routes.js
@@ -20,6 +20,8 @@ routes.put('/users', UserController.update);
 
 routes.post('/appointments', AppointmentController.store);
 
+routes.get('/appointments', AppointmentController.index);
+
 routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/providers', ProviderController.index);
