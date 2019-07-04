9549a9ff6c5691a8a77956729ea1475bba8413d5
Joshua Maia Rodrigues <joshuamaia@codata.pb.gov.br>
2019-07-04 16:10:38 -0300

    Configurando MongoDB

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 import databaseConfig from '../config/database';
 import User from '../app/models/User';
 import File from '../app/models/File';
@@ -9,6 +10,7 @@ const models = [User, File, Appointment];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
@@ -16,6 +18,16 @@ class Database {
     models.map(model => model.init(this.connection));
     models.map(
       model => model.associate && model.associate(this.connection.models)
+    );
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/gobarber',
+      {
+        useNewUrlParser: true,
+        useFindAndModify: true,
+      }
     );
   }
 }
