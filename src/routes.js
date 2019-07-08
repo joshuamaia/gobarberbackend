f5a552f743c710c76fe47fa12ad6cb6b340a8d5b
Joshua Maia Rodrigues <joshuamaia@codata.pb.gov.br>
2019-07-08 17:37:45 -0300

    Listando notificações do usuário

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -26,6 +27,8 @@ routes.get('/appointments', AppointmentController.index);
 routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/providers', ProviderController.index);
+
+routes.get('/notifications', NotificationController.index);
 
 routes.get('/schedule', ScheduleController.index);
 
