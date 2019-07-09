c69982e8008a4fbc220bda32a570f844b70a524b
Joshua Maia Rodrigues <joshuamaia@codata.pb.gov.br>
2019-07-09 16:41:32 -0300

    Configurando templates de e-mail

--- src/lib/Mail.js
+++ src/lib/Mail.js
@@ -1,4 +1,7 @@
 import nodemailer from 'nodemailer';
+import exphbs from 'express-handlebars';
+import nodemailerhbs from 'nodemailer-express-handlebars';
+import { resolve } from 'path';
 import mailConfig from '../config/mail';
 
 class Mail {
@@ -11,9 +14,29 @@ class Mail {
       secure,
       auth: auth.user ? auth : null,
     });
+
+    this.configureTemplates();
   }
 
-  async sendMail(message) {
+  configureTemplates() {
+    const viewPath = resolve(__dirname, '..', 'app', 'views', 'emails');
+
+    this.transporter.use(
+      'compile',
+      nodemailerhbs({
+        viewEngine: exphbs.create({
+          layoutsDir: resolve(viewPath, 'layouts'),
+          partialsDir: resolve(viewPath, 'partials'),
+          defaultLayout: 'default',
+          extname: '.hbs',
+        }),
+        viewPath,
+        extName: '.hbs',
+      })
+    );
+  }
+
+  sendMail(message) {
     return this.transporter.sendMail({
       ...mailConfig.default,
       ...message,
