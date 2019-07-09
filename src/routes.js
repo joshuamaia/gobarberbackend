61fba4ba43b52d3d4f2a1ac44242e1589744fd50
Joshua Maia Rodrigues <joshuamaia@codata.pb.gov.br>
2019-07-08 21:43:39 -0300

    Cancelamento de agendamento

--- src/routes.js
+++ src/routes.js
@@ -21,8 +21,8 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.post('/appointments', AppointmentController.store);
-
 routes.get('/appointments', AppointmentController.index);
+routes.delete('/appointments/:id', AppointmentController.delete);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
