bf591f9eded264bf0bd2507bec08bebf86b69ea8
Joshua Maia Rodrigues <joshuamaia@codata.pb.gov.br>
2019-07-10 10:50:08 -0300

    Listando horários disponíveis

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -27,6 +28,7 @@ routes.delete('/appointments/:id', AppointmentController.delete);
 routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
 
 routes.get('/notifications', NotificationController.index);
 
